113b582504590c3e0c0180c18d146aee465e3404
grnex <grodrigues.devloper@gmail.com>
2026-03-26 16:29:53 -0300

    att

--- backend/app.py
+++ backend/app.py
@@ -62,5 +62,6 @@ if __name__ == '__main__':
     app.run(
         host='0.0.0.0',
         port=5000,  # API na porta 5000
-        debug=True
+        debug=True,
+        use_reloader=False 
     )
